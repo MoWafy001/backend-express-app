2531932e6dcea0e32c5bf2b4a6d391f4a72a4663
mowafy001 <wafy123445@gmail.com>
2024-02-03 02:05:48 +0200

    feat: add prisma

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,23 +1,9 @@
 import { Env } from "../lib/env/env";
 
 export interface IDbConfig {
-  type: string;
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-  synchronize: boolean;
-  logging: boolean;
+  databaseUrl?: string;
 }
 
 export const dbConfig: IDbConfig = {
-  type: Env.get("DB_TYPE").toString(),
-  host: Env.get("DB_HOST").toString(),
-  port: Env.get("DB_PORT").toNumber(),
-  username: Env.get("DB_USERNAME").toString(),
-  password: Env.get("DB_PASSWORD").toString(),
-  database: Env.get("DB_DATABASE").toString(),
-  synchronize: Env.get("DB_SYNCHRONIZE", false).toBoolean(),
-  logging: Env.get("DB_LOGGING", false).toBoolean(),
+  databaseUrl: Env.get("DATABASE_URL").toString(),
 };
